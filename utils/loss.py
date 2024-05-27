937725b515f68d9bbfa333bac1a11ab1615835f2
ljwztc <296433517@qq.com>
2024-05-27 12:18:37 +0800

    update dice loss

--- utils/loss.py
+++ utils/loss.py
@@ -17,8 +17,8 @@ class BinaryDiceLoss(nn.Module):
 
     def forward(self, predict, target):
         assert predict.shape[0] == target.shape[0], "predict & target batch size don't match"
-        predict = predict.contiguous().view(predict.shape[0], -1)
-        target = target.contiguous().view(target.shape[0], -1)
+        predict = predict.contiguous().view(-1)
+        target = target.contiguous().view(-1)
 
         num = torch.sum(torch.mul(predict, target), dim=1)
         den = torch.sum(predict, dim=1) + torch.sum(target, dim=1) + self.smooth
